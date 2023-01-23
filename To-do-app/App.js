f76eca6e2d30d5ef072c1d1f2ef7419b3bfc85c6
Md salman <99859514+mohdslmn@users.noreply.github.com>
2023-01-23 20:36:05 +0530

    Add files via upload

---
+++ To-do-app/App.js
@@ -0,0 +1,40 @@
+import React ,{useState} from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodosList from "./components/TodosList";
+import './App.css';
+
+function App() {
+  const [input,setInput]=useState("");
+  const [todos,setTodos]=useState([]);
+  const [editTodo,setEditTodo]=useState("null");
+  return (
+  <div className="container">
+<div className="app-wrapper">
+  <div><Header /></div>
+  <div><Form 
+  input={input}
+  setInput={setInput}
+  todos={todos}
+  setTodos={setTodos}
+  editTodo={editTodo}
+  setEditTodo={setEditTodo}
+  /></div>
+<div>
+  <TodosList todos={todos}
+  setTodos={setTodos}
+  setEditTodo={setEditTodo}/>
+</div>
+
+
+
+
+
+</div>
+  </div>
+
+  
+  );
+}
+
+export default App;
